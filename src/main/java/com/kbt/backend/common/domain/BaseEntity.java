ac64d06d2692bfe5e5d1fd2d90dc693bc79de93e
023-dev <rhdgmltkd88@gmail.com>
2026-06-18 21:48:39 +0900

    feat: BaseEntity에 auditing 적용

--- src/main/java/com/kbt/backend/common/domain/BaseEntity.java
+++ src/main/java/com/kbt/backend/common/domain/BaseEntity.java
@@ -1,14 +1,24 @@
 package com.kbt.backend.common.domain;
 
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.experimental.Accessors;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener .class)
 @Getter
+@Accessors(fluent = true)
 public abstract class BaseEntity {
-    private final LocalDateTime createdAt;
 
-    protected BaseEntity() {
-        this.createdAt = LocalDateTime.now();
-    }
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
